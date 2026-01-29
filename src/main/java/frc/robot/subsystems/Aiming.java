20662c262713e34df32d9471e58a4f8bfc8abdfe
871swlaptop <abaranec3@gmail.com>
2026-01-28 20:51:58 -0500

    Start real math for Trajectory/Shooter

--- src/main/java/frc/robot/subsystems/Aiming.java
+++ src/main/java/frc/robot/subsystems/Aiming.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Inches;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.Units;
@@ -9,6 +14,7 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -33,7 +39,7 @@ public class Aiming extends SubsystemBase {
             setDesiredYaw();
             boolean bob = false;
             if(bob){
-            setDesiredRPM();
+                setDesiredRPM();
             }
         });
     }
@@ -41,6 +47,9 @@ public class Aiming extends SubsystemBase {
     public void setDesiredYaw() {
         Translation3d turretPose = turret.findTurretPose().getTranslation();
         Translation3d hub = Translation3d.kZero;
+
+
+
         Angle desiredYaw = null;
         turret.setYawSetpoint(desiredYaw);
     }
@@ -50,9 +59,30 @@ public class Aiming extends SubsystemBase {
         Distance height = null;
         Distance distance = null;
         Angle pitchAngle = null;
+
+
+
         LinearVelocity desiredShootSpeed = null;
+        makeFuelTrajectoryArray(desiredShootSpeed, turret.targetPoseOfFuelRelease());
         shooter.setShooterSpeed(desiredShootSpeed);
     }
 
+    public void makeFuelTrajectoryArray(LinearVelocity desiredShootSpeed, Pose3d initialPose){
+        List<Translation3d> fuelPositions = new ArrayList<>();
+        Distance ground = Units.Inches.of(0);
+        Translation3d fuelPosition = initialPose.getTranslation();
+        for(Time t = Units.Seconds.of(0); fuelPosition.getMeasureY().gte(ground) ; t = t.plus(Units.Seconds.of(.1))){
+            fuelPosition = findBallPosition(t, initialPose, desiredShootSpeed);
+            fuelPositions.add(fuelPosition); 
+        }
+        Translation3d[] fuelTrajectory = fuelPositions.toArray(size -> new Translation3d[size]);
+        Logger.recordOutput("Aiming/TrajectoryPath", fuelTrajectory);
+    }
 
+    public Translation3d findBallPosition(Time time, Pose3d initialPose, LinearVelocity launchSpeed){
+        double angle = initialPose.getRotation().getY();        // launch angle
+        LinearVelocity initialVelocityY = launchSpeed.times(Math.sin(angle));
+        LinearVelocity initialVelocityX = launchSpeed.times(Math.cos(angle));
+        return null;
+    }
 }
